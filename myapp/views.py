bf267493af30cf25cb5899fd22bdaa170a9c9e3f
Lukas <lukas.voldset@gmail.com>
2025-01-22 11:21:05 +0100

    added simple profile page

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth.forms import AuthenticationForm
 def home(request):
     form = AuthenticationForm()  # Create an instance of the login form
     return render(request, 'index.html', {'form': form})  # Pass the form to the template
+
+def profile(request):
+    return render(request, 'profile.html')
