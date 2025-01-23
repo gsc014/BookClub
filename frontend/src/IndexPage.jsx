50c9a36723d32fcdd1296a755c5cb07d6c2ff80d
agu078 <agu078@uit>
2025-01-23 13:07:31 +0100

    tried more react, have to start making components

--- frontend/src/IndexPage.jsx
+++ frontend/src/IndexPage.jsx
@@ -1,29 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import brightmode from './assets/bright-mode.png'
 
 const IndexPage = () => {
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000')
-      .then(response => setData(response.data))
-      .catch(error => console.error('Error fetching home data:', error));
-  }, []);
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000')
+            .then(response => setData(response.data))
+            .catch(error => console.error('Error fetching home data:', error));
+    }, []);
 
-  if (!data) return <div>Loading...</div>;
+    if (!data) return <div>Loading...</div>;
 
-  return (
-    <div>
-      <h1>{data.message}</h1>
-      <form>
-        <label>{data.login_form.username}</label>
-        <input type="text" placeholder="Username" />
-        <label>{data.login_form.password}</label>
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <header id="header">
+            <div className="logo">
+                <a href="" style={{ color: "#ffffff", textDecoration: "none" }}>
+                    Book Club
+                </a>
+            </div>
+            <div id="useful_buttons">
+                {/* <button id="dark_mode" class="button">Eyes hurting?</button> */}
+                <img
+                    src={brightmode}
+                    id="dark_mode"
+                    className="icon jump"
+                />
+                {/* <img src="{% static 'pictures/user.png' %}" id="User" class="icon jump"> */}
+                <a href="{% url 'profile' %}">
+                    <img
+                        src="{% static 'pictures/user.png' %}"
+                        id="User"
+                        className="icon jump"
+                    />
+                </a>
+                <a href="{% url 'settings' %}">
+                    <img
+                        src="{% static 'pictures/settings_icon.png' %}"
+                        id="Settings"
+                        className="icon spin"
+                    />
+                </a>
+            </div>
+
+            <div>
+                <h1>{data.message}</h1>
+                <form>
+                    <label>{data.login_form.username}</label>
+                    <input type="text" placeholder="Username" />
+                    <label>{data.login_form.password}</label>
+                    <input type="password" placeholder="Password" />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </header>
+    );
 };
 
 export default IndexPage;
