ab2d51345e81a45a0120d61099d22e627423bc55
gsc014 <gsc014@uit.no>
2025-02-12 13:48:02 +0100

    modified:   db.sqlite3 	modified:   frontend/src/assets/bookcard.css 	modified:   frontend/src/assets/bookcard.jsx 	new file:   frontend/src/assets/pictures/diskette.png 	new file:   frontend/src/assets/pictures/diskette_saved.png 	new file:   frontend/src/assets/pictures/file.png 	new file:   frontend/src/assets/pictures/information.png 	new file:   frontend/src/assets/pictures/no-results.png 	new file:   myapp/migrations/0001_initial.py 	modified:   myapp/models.py 	modified:   myapp/static/css/stylesheet.css 	modified:   myapp/urls.py 	modified:   myapp/views.py 	modified:   mysite/settings.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import login_user, signup_user
+from .views import login_user, signup_user, random_book
 
 
 urlpatterns = [
     path('profile/', views.profile, name='profile_api'),
-    path('settings/', views.settings, name='settings_api'),
-    path('', views.home, name='home'),
+    # path('settings/', views.settings, name='settings_api'),
+    # path('', views.home, name='home'),
     path('api/login/', login_user, name='login_user'),
     path('api/signup/', signup_user, name='signup_user'),
-
-        
-
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('profile/', views.profile, name='profile'),
-    # path('settings/', views.settings, name='settings'),
+    path('random-book/', random_book, name='random_book'),
 ]
