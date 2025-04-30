72f3e32c234ca21f0127b229f1aba9928f83d7e2
gsc014 <gard@LAPTOP-82FMHP9R>
2025-04-30 15:13:11 +0200

    modified:   myapp/tests/tests.py 	modified:   requirements.txt

--- myapp/tests/tests.py
+++ myapp/tests/tests.py
@@ -1,3 +1,5 @@
+import collections.abc
+collections.Callable= collections.abc.Callable
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
