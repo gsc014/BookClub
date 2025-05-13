7557e919f352fdf3be34cef5c9be65bd39b3f583
Lukas <lukas.voldset@gmail.com>
2025-05-13 16:39:59 +0200

    .

--- frontend/src/__tests__/signin.test.tsx
+++ frontend/src/__tests__/signin.test.tsx
@@ -1,5 +1,3 @@
-
-import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { describe, it, expect, vi, beforeEach, Mocked} from 'vitest';
 import axios, { AxiosStatic } from 'axios';
